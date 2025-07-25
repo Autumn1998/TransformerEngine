8580a4cf018e523edc24f35e5e7edf23c3bf9eb0
tongliu <tongliu@nvidia.com>
2025-07-24 22:21:43 -0700

    fix

--- transformer_engine/pytorch/module/grouped_linear.py
+++ transformer_engine/pytorch/module/grouped_linear.py
@@ -174,15 +174,24 @@ class _GroupedLinear(torch.autograd.Function):
 
         if torch.distributed.get_rank() == 0:
             for i in range(num_gemms):
-                print("weights_fp8[%d]._rowwise_data: " % i, weights_fp8[i]._rowwise_data.shape)
-                print("weights_fp8[%d]._columnwise_data: " % i, weights_fp8[i]._columnwise_data.shape)
-                print("weights_fp8[%d]._rowwise_scale_inv: " % i, weights_fp8[i]._rowwise_scale_inv.shape)
-                print("weights_fp8[%d]._columnwise_scale_inv: " % i, weights_fp8[i]._columnwise_scale_inv.shape)
-                print("inputmats[%d]._rowwise_data: " % i, inputmats[i]._rowwise_data.shape)
-                print("inputmats[%d]._columnwise_data: " % i, inputmats[i]._columnwise_data.shape)
-                print("inputmats[%d]._rowwise_scale_inv: " % i, inputmats[i]._rowwise_scale_inv.shape)
-                print("inputmats[%d]._columnwise_scale_inv: " % i, inputmats[i]._columnwise_scale_inv.shape)
-                print("out: ", out.shape)
+                if weights_fp8[i]._rowwise_data is not None:
+                    print("weights_fp8[%d]._rowwise_data: " % i, weights_fp8[i]._rowwise_data.shape)
+                if weights_fp8[i]._columnwise_data is not None:
+                    print("weights_fp8[%d]._columnwise_data: " % i, weights_fp8[i]._columnwise_data.shape)
+                if weights_fp8[i]._rowwise_scale_inv is not None:
+                    print("weights_fp8[%d]._rowwise_scale_inv: " % i, weights_fp8[i]._rowwise_scale_inv.shape)
+                if weights_fp8[i]._columnwise_scale_inv is not None:
+                    print("weights_fp8[%d]._columnwise_scale_inv: " % i, weights_fp8[i]._columnwise_scale_inv.shape)
+                if inputmats[i]._rowwise_data is not None:
+                    print("inputmats[%d]._rowwise_data: " % i, inputmats[i]._rowwise_data.shape)
+                if inputmats[i]._columnwise_data is not None:
+                    print("inputmats[%d]._columnwise_data: " % i, inputmats[i]._columnwise_data.shape)
+                if inputmats[i]._rowwise_scale_inv is not None:
+                    print("inputmats[%d]._rowwise_scale_inv: " % i, inputmats[i]._rowwise_scale_inv.shape)
+                if inputmats[i]._columnwise_scale_inv is not None:
+                    print("inputmats[%d]._columnwise_scale_inv: " % i, inputmats[i]._columnwise_scale_inv.shape)
+                if out is not None:
+                    print("out: ", out.shape)
 
         # Perform GEMM
         _ = general_grouped_gemm(
